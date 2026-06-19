14aa3b2f9f6882245e4bf2556febec5b27d9a012
Rabbiah5 <rabbiahqamar5@gmail.com>
2026-06-19 18:34:07 +0500

    indusrty icons added

---
+++ assets/icons/industry/Gemini.tsx
@@ -0,0 +1,88 @@
+function Gemini() {
+  return (
+    <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g filter="url(#filter0_iiii_577_5646)">
+        <path
+          d="M0 20C0 8.95431 8.95431 0 20 0C31.0457 0 40 8.95431 40 20C40 31.0457 31.0457 40 20 40C8.95431 40 0 31.0457 0 20Z"
+          fill="#EB692C"
+        />
+        <path
+          d="M20.0913 25.088C19.9886 24.4813 19.7179 23.828 19.2793 23.128C18.8406 22.4187 18.2153 21.7607 17.4033 21.154C16.6006 20.5473 15.7979 20.16 14.9953 19.992V19.404C15.7886 19.2173 16.5493 18.872 17.2773 18.368C18.0146 17.8547 18.6306 17.2387 19.1253 16.52C19.6293 15.7827 19.9513 15.0547 20.0913 14.336H20.6793C20.7633 14.8027 20.9313 15.2833 21.1833 15.778C21.4353 16.2633 21.7573 16.73 22.1493 17.178C22.5506 17.6167 22.9986 18.0133 23.4933 18.368C24.2306 18.8907 24.9819 19.236 25.7473 19.404V19.992C25.2339 20.0947 24.7019 20.3047 24.1513 20.622C23.6099 20.9393 23.1059 21.3173 22.6393 21.756C22.1726 22.1853 21.7899 22.638 21.4913 23.114C21.0526 23.814 20.7819 24.472 20.6793 25.088H20.0913Z"
+          fill="white"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_iiii_577_5646"
+          x="0"
+          y="0"
+          width="40"
+          height="48.4007"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="8.40074" />
+          <feGaussianBlur stdDeviation="8.40074" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.03 0" />
+          <feBlend mode="normal" in2="shape" result="effect1_innerShadow_577_5646" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="7.19987" />
+          <feGaussianBlur stdDeviation="2.4004" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.02 0" />
+          <feBlend
+            mode="normal"
+            in2="effect1_innerShadow_577_5646"
+            result="effect2_innerShadow_577_5646"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="1.19953" />
+          <feGaussianBlur stdDeviation="0.599767" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.1 0" />
+          <feBlend
+            mode="normal"
+            in2="effect2_innerShadow_577_5646"
+            result="effect3_innerShadow_577_5646"
+          />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="1.09012" />
+          <feGaussianBlur stdDeviation="0.545061" />
+          <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+          <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.25 0" />
+          <feBlend
+            mode="normal"
+            in2="effect3_innerShadow_577_5646"
+            result="effect4_innerShadow_577_5646"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+}
+
+export default Gemini;
