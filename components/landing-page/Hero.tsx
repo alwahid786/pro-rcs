145f994d0b6f3136b156c3bacfd916564e886aeb
moizxox <mianmoiz899@gmail.com>
2026-06-19 12:53:48 +0500

    services elements

--- components/landing-page/Hero.tsx
+++ components/landing-page/Hero.tsx
@@ -9,17 +9,17 @@ import RoatatingStar from "../ui/RoatatingStar";
 const highlightColors = ["#eb692c", "#f08a52", "#e57638"];
 
 const HeroGrad = ({ position }: { position: string }) => {
-  return <div className={`absolute ${position} h-150 w-100 rounded-full bg-[#C04A12]/80 blur-[144px] animate-pulse [animation-duration:5s]`} />;
+  return <div className={`absolute ${position} h-150 w-100 rounded-full bg-[#C04A12]/60 blur-[144px] animate-pulse [animation-duration:5s]`} />;
 };
 
 const Hero = () => {
   return (
     <section className="relative bg-white z-10">
-      <HeroGrad position="top-0 left-[-20%]" />
-      <HeroGrad position="top-0 right-[-20%]" />
-      <RoatatingStar position="top-0 right-0" width="70" />
+      <HeroGrad position="lg:top-0 lg:left-[-20%] max-lg:hidden" />
 
-      <div className="container relative flex flex-col items-center px-5 py-16 text-center sm:py-20 lg:py-24 ">
+      <HeroGrad position="top-[-60%] right-1/2 translate-x-1/2 opacity-50 lg:top-0 lg:right-[-20%] lg:translate-x-0 lg:opacity-100" />
+      <RoatatingStar position="top-0 right-0" width="70" />
+      <div className="container relative flex z-2 flex-col items-center px-5 py-16 text-center sm:py-20 lg:py-24 ">
         <span className="max-w-7xl font-bold relative ">
           <RoatatingStar position="top-[-10px] left-[70px]" width="30" />
           <Heading as="h1" align="center" className=" z-10 relative">
--- components/ui/RoatatingStar.tsx
+++ components/ui/RoatatingStar.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import star from "@/assets/imgs/star.png";
 
-const RoatatingStar = ({ position, width }: { position: string; width: string | number }) => {
+const RoatatingStar = ({ position, width, className }: { position: string; width: string | number; className?: string }) => {
   const size = Number(width);
 
   return (
-    <div className={`absolute ${position} z-0 opacity-50`} style={{ width: size, height: size }}>
+    <div className={`absolute ${position} z-0 opacity-50 ${className}`} style={{ width: size, height: size }}>
       <Image src={star} width={size} height={size} className="animate-spin [animation-duration:28s] h-full w-full" alt="star" />
     </div>
   );
