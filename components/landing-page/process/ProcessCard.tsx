7b7debaaa4add4fabb773cb18808c4e84b4d0646
moizxox <mianmoiz899@gmail.com>
2026-06-20 15:29:57 +0500

    process secton

--- components/landing-page/process/ProcessCard.tsx
+++ components/landing-page/process/ProcessCard.tsx
@@ -4,15 +4,15 @@ import type { ProcessStep } from "./data";
 
 const ProcessCard = ({ number, title, description }: ProcessStep) => {
   return (
-    <GradientCard className="p-6 sm:p-8">
-      <div className="relative z-10 flex h-full flex-col">
-        <Heading as="p" size="step-number" aria-hidden className="text-[#F9CEBA80]">
+    <GradientCard className="p-6 sm:p-8 ">
+      <div className="relative z-10 flex pb-10 h-full flex-col max-w-[90%]">
+        <Heading as="p" size="step-number" aria-hidden className="text-[#F9CEBA80] text-[180px]!">
           {number}
         </Heading>
 
-        <Heading as="h3" size="card-title" weight="bold" multicolor={title} className="mt-6" uppercase={false} />
+        <Heading as="h3" size="card-title" weight="bold" multicolor={title} className="mt-6 text-[42px]!" uppercase={false} />
 
-        <p className="mt-3 font-sans text-base font-medium leading-[142%] text-text-secondary">{description}</p>
+        <p className="mt-3 font-sans text-xl font-medium leading-[142%] text-text-secondary">{description}</p>
       </div>
     </GradientCard>
   );
